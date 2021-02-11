156ee8373b4edfd9d25048a9b44b0d5625ff6d22
Christopher Kim <chris.kim1990@gmail.com>
2021-02-11 16:58:21 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,11 +1,11 @@
 def join_nested_strings(src)
-  array_of_string = []
+  mixed_data_2 = []
   row_index = 0
   while row_index < src.length do
     element_index = 0
     while element_index < src[row_index].length do
-      if src[row_index][element_index] == ""
-        array_of_string << src[row_index][element_index]
+      if src[row_index][element_index] == .to_s
+        mixed_data_2 << src[row_index][element_index]
       end
     element_index += 1
     end
