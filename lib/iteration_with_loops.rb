5cae9ddaf2b40fffceab325b50be849d9e272645
Christopher Kim <chris.kim1990@gmail.com>
2021-02-11 17:05:03 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ def join_nested_strings(src)
     element_index = 0
     while element_index < src[row_index].length do
       if src[row_index][element_index].class == String
-        phrase = src[row_index][element_index]
+        phrase = src[row_index][element_index].join
         new_array << phrase
       end
     element_index += 1
